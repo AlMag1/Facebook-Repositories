598cb5e02036ac925742cfab818f95f42f6084bd
Aris Alexopoulos <alex.aris90@gmail.com>
2020-09-19 16:46:17 +0300

    [feat] Implement loader

--- src/views/github-repos/Loader.js
+++ src/views/github-repos/Loader.js
@@ -6,16 +6,26 @@ import facebook from '../../assets/svg/facebook.svg';
 
 const Loader = () => {
   return (
-    <Row className="no-gutters align-items-center github-repos__loading">
-      <Col xs={'auto'} className="d-flex mr-2">
-        <SvgRender src={facebook} style={{ width: 40, height: 40 }} className="text-primary" />
-      </Col>
-      <Col xs={'auto'} className="github-repos__loading-repository">
-        <div>Repository</div>
-      </Col>
-      <Col xs={12} className="d-flex align-items-center">
-        <Spinner color="warning" />
-        <div>loading</div>
+    <Row className="no-gutters github-repos__loading">
+      <Col>
+        <Row className="no-gutters align-items-center justify-content-center mb-4">
+          <Col xs={'auto'} className="d-flex">
+            <SvgRender
+              src={facebook}
+              style={{ width: 76, height: 76 }}
+              className="github-repos__loading-facebook"
+            />
+          </Col>
+          <Col xs={'auto'} className="github-repos__loading-repository">
+            <div>Repository</div>
+          </Col>
+        </Row>
+        <Row className="no-gutters align-items-center justify-content-center">
+          <Col xs={12} className="d-flex align-items-center justify-content-center">
+            <Spinner color="warning" />
+            <div className="ml-3 font-weight-semibold spinner-loading">loading</div>
+          </Col>
+        </Row>
       </Col>
     </Row>
   );
